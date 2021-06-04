06f463c26dab2c48d39004ff4d16ef796f547b8d
Yurun <admin@yurunsoft.com>
2021-06-04 17:11:57 +0800

    重构 ConnectContext 为 ConnectionContext 砍掉 ConnectionBinder 加入 ConnectionContext 更新一波文档

---
+++ tests/Web/config/config.php
@@ -0,0 +1,110 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    // 项目根命名空间
+    'namespace'    => 'Imi\Fpm\Test\Web',
+
+    'debug'       => false,
+
+    // 配置文件
+    'configs'    => [
+        'beans'        => __DIR__ . '/beans.php',
+    ],
+
+    // 扫描目录
+    'beanScan'    => [
+        'Imi\Fpm\Test\Web\Controller',
+        'Imi\Fpm\Test\Web\Middleware',
+        'Imi\Fpm\Test\Web\Error',
+    ],
+
+    // 组件命名空间
+    'components'    => [
+        'Fpm' => 'Imi\Fpm',
+    ],
+
+    'ignoreNamespace'   => [
+        'Imi\Swoole\*',
+        'Imi\Workerman\*',
+        'Imi\Cron\*',
+    ],
+
+    'ignorePaths'   => [
+        dirname(__DIR__) . \DIRECTORY_SEPARATOR . 'public',
+    ],
+
+    // 日志配置
+    'logger' => [
+        'channels' => [
+            'imi' => [
+                'handlers' => [
+                    [
+                        'class'     => \Monolog\Handler\RotatingFileHandler::class,
+                        'construct' => [
+                            'filename' => dirname(__DIR__) . '/logs/log.log',
+                        ],
+                        'formatter' => [
+                            'class'     => \Monolog\Formatter\LineFormatter::class,
+                            'construct' => [
+                                'dateFormat'                 => 'Y-m-d H:i:s',
+                                'allowInlineLineBreaks'      => true,
+                                'ignoreEmptyContextAndExtra' => true,
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+        ],
+    ],
+
+    // 数据库配置
+    'db'    => [
+        // 默认连接池名
+        'defaultPool'    => 'maindb',
+    ],
+
+    // redis 配置
+    'redis' => [
+        // 默认连接池名
+        'defaultPool'   => 'redis',
+    ],
+
+    // 锁
+    'lock'  => [
+        'list'  => [
+            // 'atomic' =>  [
+            //     'class' =>  'AtomicLock',
+            //     'options'   =>  [
+            //         'atomicName'    =>  'atomicLock',
+            //     ],
+            // ],
+            'memoryTableLock' => [
+                'class'     => 'RedisLock',
+                'options'   => [
+                    'poolName'  => 'redis',
+                ],
+            ],
+            'redisConnectionContextLock' => [
+                'class'     => 'RedisLock',
+                'options'   => [
+                    'poolName'  => 'redis',
+                ],
+            ],
+        ],
+    ],
+
+    // atmoic 配置
+    'atomics'    => [
+        'atomicLock'   => 1,
+    ],
+
+    'middleware'    => [
+        'groups'    => [
+            'test'  => [
+                \Imi\Fpm\Test\Web\Middleware\Middleware4::class,
+            ],
+        ],
+    ],
+];
