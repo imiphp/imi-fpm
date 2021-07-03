10dcb044497fba1c8705ffff636cc4c91dcd689d
Yurun <admin@yurunsoft.com>
2021-07-03 13:25:41 +0800

    组件注解兼容，修复问题

---
+++ src/Runtime/Handler/FpmRuntimeModeHandler.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Imi\Fpm\Runtime\Handler;
+
+use Imi\App;
+use Imi\Bean\Annotation\Bean;
+use Imi\Config;
+use Imi\Core\Runtime\Contract\IRuntimeModeHandler;
+use Imi\Event\Event;
+
+/**
+ * @Bean("FpmRuntimeModeHandler")
+ */
+class FpmRuntimeModeHandler implements IRuntimeModeHandler
+{
+    /**
+     * 初始化.
+     */
+    public function init(): void
+    {
+        if (!App::isDebug())
+        {
+            foreach ([
+                '@app.imi.runtime.annotation_manager_annotations',
+                '@app.imi.runtime.annotation_manager_annotation_relation',
+            ] as $name)
+            {
+                if (null === Config::get($name))
+                {
+                    Config::set($name, false);
+                }
+            }
+        }
+        Event::on('IMI.BUILD_RUNTIME', \Imi\Fpm\Server\Http\Listener\BuildRuntimeListener::class);
+        Event::on('IMI.LOAD_RUNTIME_INFO', \Imi\Fpm\Server\Http\Listener\LoadRuntimeListener::class);
+    }
+}
